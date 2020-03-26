ae82dbbbafc9ecc21e3b2767eed0951e2d6e8f0a
Phil Howard <phil@gadgetoid.com>
2020-03-26 15:15:42 +0000

    Mock GPIO for Matrix test

--- library/tests/test_matrix.py
+++ library/tests/test_matrix.py
@@ -1,10 +1,10 @@
-def test_setup_matrix(config_file, rpi_ws281x, serial):
+def test_setup_matrix(config_file, GPIO, rpi_ws281x, serial):
     from plasma.matrix import PlasmaMatrix
     plasma = PlasmaMatrix(config_file)
     assert plasma.get_pixel_count() == 100
     assert plasma.get_device_count() == 3
 
-def test_show_matrix(config_file, rpi_ws281x, serial):
+def test_show_matrix(config_file, GPIO, rpi_ws281x, serial):
     from plasma.matrix import PlasmaMatrix
     plasma = PlasmaMatrix(config_file)
     plasma.show()
