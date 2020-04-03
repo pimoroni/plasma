22ba12e740040b8d2bc02237baffbdfb21ea98b4
Phil Howard <phil@gadgetoid.com>
2020-04-03 11:51:24 +0100

    Drop defunct classifiers var

--- library/setup.py
+++ library/setup.py
@@ -24,14 +24,4 @@ SOFTWARE.
 
 from setuptools import setup
 
-classifiers = ['Development Status :: 5 - Production/Stable',
-               'Operating System :: POSIX :: Linux',
-               'License :: OSI Approved :: MIT License',
-               'Intended Audience :: Developers',
-               'Programming Language :: Python :: 2.6',
-               'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3',
-               'Topic :: Software Development',
-               'Topic :: System :: Hardware']
-
 setup()
