fd0de3acaedc869a6dc8f3a1ffba173a318298ef
Phil Howard <github@gadgetoid.com>
2025-03-24 14:35:07 +0000

    Fix default I2C pins.

--- boards/plasma_2040/mpconfigboard.h
+++ boards/plasma_2040/mpconfigboard.h
@@ -4,8 +4,8 @@
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (1024 * 1024))
 
 // I2C0 (non-default)
-#define MICROPY_HW_I2C0_SCL                     (21)
 #define MICROPY_HW_I2C0_SDA                     (20)
+#define MICROPY_HW_I2C0_SCL                     (21)
 
 // There is no good SPI bus broken out on Plasma 2040
 // So disable default pins and force the user to pick
